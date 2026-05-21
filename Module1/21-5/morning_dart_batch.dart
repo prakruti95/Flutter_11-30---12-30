b01d6986970af77033dc06dde85f4aef84ffad9c
Prakruti <prakrutibook@gmail.com>
2026-05-21 12:25:00 +0530

    commit

---
+++ Module1/21-5/morning_dart_batch.dart
@@ -0,0 +1,6 @@
+import 'package:morning_dart_batch/morning_dart_batch.dart' as morning_dart_batch;
+
+void main(List<String> arguments)
+{
+  print('Hello world: ${morning_dart_batch.calculate()}!');
+}
