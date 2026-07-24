4f96254d5b2597215a1dcd7e06a920e5b058382b
Prakruti <prakrutibook@gmail.com>
2026-07-24 11:05:05 +0530

    commit

---
+++ Module4-Db/24-7/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:sqfliteex1/view.dart';
+
+void main()
+{
+  runApp(MaterialApp(home:ViewData()));
+}
+
---
+++ Module4-Db/24-7/view.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:sqfliteex1/dbhelper.dart';
+
+import 'add.dart';
+
+class ViewData extends StatefulWidget
+{
+  const ViewData({super.key});
+
+  @override
+  State<ViewData> createState() => _ViewDataState();
+}
+
+class _ViewDataState extends State<ViewData>
+{
+  late Dbhelper dbhelper = Dbhelper();
+  List<Map> slist = [];
+  @override
+  void initState()
+  {
+    dbhelper.open();
+    getdata();
+  }
+
+  @override
+  Widget build(BuildContext context)
+  {
+    return Scaffold
+      (
+        appBar: AppBar(title: Text("View Data"),),
+        body: Center
+          (
+          child: SingleChildScrollView(
+            child: Column
+              (
+                children:slist.map((tops)
+                {
+            
+                  return Card
+                    (
+                      child: ListTile
+                        (
+                        leading:Icon(Icons.person),
+                        title: Text(tops["name"]),
+                        subtitle:Text(tops["email"]),
+                        trailing: Wrap(children:
+                        [
+                          IconButton(onPressed: (){}, icon: Icon(Icons.edit)),
+                          IconButton(onPressed: (){}, icon: Icon(Icons.delete))
+                        ],),
+                      )
+                  );
+                }).toList()
+            
+            ),
+          ),),
+    floatingActionButton: FloatingActionButton(onPressed: ()
+    {
+    Navigator.pushReplacement(context,MaterialPageRoute(builder: (context) => AddData()));
+    },child: Icon(Icons.add),));
+  }
+
+  getdata()
+  {
+    Future.delayed(Duration(milliseconds: 500),()async
+    {
+      slist = await dbhelper.db.rawQuery('SELECT * FROM students');
+      setState(()
+      {
+
+      });
+    });
+  }
+}
